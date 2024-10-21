b348abdc00d7929fd9b261d24a6ef37e5c6f9c96
vhratts <118541950+vratts@users.noreply.github.com>
2024-10-21 13:14:31 -0300

    Atualização 1.1.5 - Stable version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import ProviderInterface from "./interfaces/ProviderInterface";
-import ThinderPixInterface from "./interfaces/ThunderPixInterface";
+import ProviderInterface from './interfaces/ProviderInterface';
+import ThinderPixInterface from './interfaces/ThunderPixInterface';
 import PixProvider from './providers/pix/PixProvider';
 import PrimepagProvider from './providers/pix/PrimepagProvider';
 import MercadoPagoProvider from './providers/pix/MercadoPagoProvider';
@@ -10,24 +10,54 @@ import CieloProvider from './providers/pix/CieloProvider';
 import EfiPayProvider from './providers/pix/EfiPayProvider';
 
 export {
-    PixProvider, PrimepagProvider, MercadoPagoProvider,
-    PicPayProvider, PagarMeProvider, OpenPixProvider,
-    CieloProvider, EfiPayProvider
+    PixProvider,
+    PrimepagProvider,
+    MercadoPagoProvider,
+    PicPayProvider,
+    PagarMeProvider,
+    OpenPixProvider,
+    CieloProvider,
+    EfiPayProvider,
 };
 
 export default class ThunderPix implements ThinderPixInterface {
     private provider: ProviderInterface;
-    constructor(provider: ProviderInterface){
+    constructor(provider: ProviderInterface) {
         this.provider = provider;
     }
 
-    async createQrCode(params?: {
-        valueCents: number,
-        expires: number
-    }) {
+    async createQrCode(params?: { valueCents: number; expires: number }) {
         return await this.provider.generatingPixBilling(params);
     }
-    async getBalance(params?: {}) {}
-    async getTransactions(params?: {}) {}
-    async getQrCode(params?: {}) {}
+
+    async getBalance() {
+        return this.provider.getBalance();
+    }
+
+    async getTransactions(params: {
+        type: string;
+        options: {
+            page: number | null;
+            registrationDateStart: string | null;
+            registrationDateEnd: string | null;
+        };
+    }) {
+        if ((params.type = 'output')) {
+            var response = await this.provider.listingPixBilling(
+                params?.options,
+            );
+        } else {
+            var response = await this.provider.listProviderWidthdraw(
+                params?.options,
+            );
+        }
+
+        return response;
+    }
+
+    async getQrCode(params: {
+        reference: string
+    }) {
+        return await this.provider.searchPixBilling(params);
+    }
 }
