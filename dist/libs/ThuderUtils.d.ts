917057132ebb79463695a53f43be3c9ae127947c
vhratts <118541950+vratts@users.noreply.github.com>
2025-01-03 22:25:03 -0300

    ✅ Atualização de utilitarios #4

--- dist/libs/ThuderUtils.d.ts
+++ dist/libs/ThuderUtils.d.ts
@@ -1,6 +1,26 @@
+interface EMVItem {
+    ID: string;
+    EMVName: string;
+    Size: number;
+    Data: string | EMVObject;
+}
+interface EMVObject {
+    [key: string]: EMVItem;
+}
 export default class ThunderUtils {
     decodeJWT(token: string): {
         header: object;
         payload: string | object;
     } | null;
+    decodeEMVToObject(emvString: string): EMVObject;
+    emvParser(emvObject: EMVObject): {
+        [key: string]: any;
+    };
+    extractPixUrl(keyValueObject: {
+        [key: string]: any;
+    }): {
+        url: string;
+        size: number;
+    } | null;
 }
+export {};
--- dist/libs/ThuderUtils.js
+++ dist/libs/ThuderUtils.js
@@ -1,5 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const EMV_NAMES = {
+    '00': 'Payload Format Indicator',
+    '26': 'Merchant Account Information',
+    '52': 'Merchant Category Code',
+    '53': 'Transaction Currency',
+    '58': 'Country Code',
+    '59': 'Merchant Name',
+    '60': 'Merchant City',
+    '62': 'Additional Data Field Template',
+    '63': 'CRC',
+    '26.00': 'GUI',
+};
 class ThunderUtils {
     decodeJWT(token) {
         try {
@@ -22,6 +34,66 @@ class ThunderUtils {
             console.error('Erro ao decodificar o JWT:', error);
             return null;
         }
+    }
+    decodeEMVToObject(emvString) {
+        const decodeField = (str) => {
+            const result = {};
+            let i = 0;
+            while (i < str.length) {
+                const id = str.slice(i, i + 2);
+                i += 2;
+                const size = parseInt(str.slice(i, i + 2), 10);
+                i += 2;
+                const data = str.slice(i, i + size);
+                i += size;
+                const emvName = EMV_NAMES[id] || '';
+                if (id === '26') {
+                    result[id] = {
+                        ID: id,
+                        EMVName: emvName,
+                        Size: size,
+                        Data: decodeField(data),
+                    };
+                }
+                else {
+                    result[id] = {
+                        ID: id,
+                        EMVName: emvName,
+                        Size: size,
+                        Data: data,
+                    };
+                }
+            }
+            return result;
+        };
+        return decodeField(emvString);
+    }
+    emvParser(emvObject) {
+        const result = {};
+        const parseItem = (item) => {
+            if (typeof item.Data === 'object') {
+                return Object.values(item.Data).map((subItem) => parseItem(subItem));
+            }
+            return item.Data;
+        };
+        for (const key in emvObject) {
+            const item = emvObject[key];
+            const keyName = (item.EMVName || `Field_${key}`).replace(/[^a-zA-Z0-9]/g, '_');
+            result[keyName] = parseItem(item);
+        }
+        return result;
+    }
+    extractPixUrl(keyValueObject) {
+        const pixKey = 'Merchant_Account_Information';
+        if (!keyValueObject[pixKey] || !Array.isArray(keyValueObject[pixKey])) {
+            return null;
+        }
+        const urls = keyValueObject[pixKey].filter((item) => /^([a-z0-9\-]+\.)+[a-z]{2,}\/.+$/.test(item));
+        if (urls.length === 0) {
+            return null;
+        }
+        const url = `https://${urls[0]}`;
+        return { url, size: url.length };
     }
 }
 exports.default = ThunderUtils;
